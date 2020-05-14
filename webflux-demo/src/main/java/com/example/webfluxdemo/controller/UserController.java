6112b41860ac4612a7b828776987d64cb8d95d8f
kinsprite <qxqs11@126.com>
2020-05-14 17:47:30 +0800

    remove unused import

--- webflux-demo/src/main/java/com/example/webfluxdemo/controller/UserController.java
+++ webflux-demo/src/main/java/com/example/webfluxdemo/controller/UserController.java
@@ -2,7 +2,6 @@ package com.example.webfluxdemo.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
